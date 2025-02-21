e33451c499fd74c049252ce1e3d68d24dfaea7d4
Resul <faridgrandov33@gmail.com>
2025-02-21 19:15:38 +0400

    salam

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import  { useState, useEffect } from 'react';
+
+function App() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((response) => response.json())
+      .then((data) => {
+        setCharacters(data.results);
+        setLoading(false);  
+      })
+      
+  }, []); 
+
+  if (loading) {
+    return <div className="text-center py-20 text-xl">Loading...</div>;
+  }
+
+
+
+  return (
+    <div className="min-h-screen bg-black p-5">
+      <h1 className="text-3xl font-bold text-center mb-8 text-white">Rick and Morty Characters</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {characters.map((character) => (
+          <div className="bg-black rounded-lg shadow-lg overflow-hidden">
+            <div className="p-3 border-2 border-solid">
+            <img 
+              src={character.image}
+              alt={character.name}
+              className=" rounded-full h-48 m-auto object-cover"
+            />
+              <h2 className="text-xl font-semibold text-white">{character.name}</h2>
+              <p className="text-sm text-white">Species: {character.species}</p>
+              <p className="text-sm text-white">Status: {character.status}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
